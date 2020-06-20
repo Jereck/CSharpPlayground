da8144d41d262a50a1a7f93cdbf049b79ebe6743
Jake Reck <jakereck@gmail.com>
2020-06-19 17:13:31 -0700

    Initial Commit

---
+++ CSharpPlayground/Playground.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpPlayground
+{
+    class Playground
+    {
+        public static void Main()
+        {
+            Console.WriteLine("This is working!");
+        }
+    }
+}
